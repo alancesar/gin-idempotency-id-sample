99db48d2bfff3b700131074c9716ea1eb5612d4a
Alan Cesar <alancesarelias@gmail.com>
2022-02-12 10:51:22 -0300

    Add unit tests

---
+++ internal/api/middleware/idempotency_test.go
@@ -0,0 +1,44 @@
+package middleware
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestDefaultKeyFn(t *testing.T) {
+	type args struct {
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+		want interface{}
+	}{
+		{
+			name: "Should create key properly",
+			args: args{
+				r: &http.Request{
+					RequestURI: "https://some-url.com",
+					Header: http.Header{
+						"Idempotency-Id": []string{"some-id"},
+					},
+				},
+			},
+			want: struct {
+				Key string
+				URL string
+			}{
+				Key: "some-id",
+				URL: "https://some-url.com",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DefaultKeyFn(tt.args.r); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DefaultKeyFn() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
